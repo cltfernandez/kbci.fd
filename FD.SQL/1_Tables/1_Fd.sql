fb642f2db169a3289382c49bc44b4023cbf700cd
cltfernandez <cltfernandez@gmail.com>
2016-05-13 08:18:37 +0800

    Signed-off-by: cltfernandez <cltfernandez@gmail.com>

--- FD.SQL/1_Tables/1_Fd.sql
+++
@@ -1,9 +0,0 @@
-﻿IF EXISTS(SELECT 1
-          FROM   INFORMATION_SCHEMA.COLUMNS
-          WHERE  TABLE_NAME = 'fd'
-                 AND COLUMN_NAME = 'ref') 
-BEGIN
-	alter table fd
-	alter column ref Varchar(20)
-END                 
-GO
